b40942c243aa6fbf9271acbc32a5299a94e9556e
anurag8484 <anuragpoonia.ap@gmail.com>
2026-01-18 20:31:58 +0530

    deploy: contracts

---
+++ contracts/src/Staking.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./ApCoin.sol";
+
+contract Staking is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 public totalStaked;
+
+    mapping(address => uint256) stakeBalance;
+    mapping(address => uint256) unStakeTime;
+    mapping(address => uint256) userRewards;
+    mapping(address => uint256) lastUpdated;
+
+    ApCoin public rewardToken;
+
+    uint256[50] private __gap;
+
+    function initialize(address _rewardToken) public initializer {
+        __Ownable_init(msg.sender);
+        rewardToken = ApCoin(_rewardToken);
+    }
+
+    function _updateReward(address user) internal {
+        uint256 staked = stakeBalance[user];
+        if (staked == 0) {
+            lastUpdated[user] = block.timestamp;
+            return;
+        }
+
+        uint256 timeElapsed = block.timestamp - lastUpdated[user];
+        uint256 reward = (staked * timeElapsed * 1e18) / (1e14 * 1 days);
+        userRewards[user] += reward;
+        lastUpdated[user] = block.timestamp;
+    }
+
+    function claimReward() external {
+        require(
+            block.timestamp >= unStakeTime[msg.sender],
+            "lock not finished"
+        );
+        _updateReward(msg.sender);
+
+        uint256 reward = userRewards[msg.sender];
+        require(reward > 0, "No claimable reward yet");
+        userRewards[msg.sender] = 0;
+        rewardToken.mint(msg.sender, reward);
+    }
+
+    function stake(uint256 lockDuration) external payable {
+        require(msg.value > 0, "Zero Stake");
+        // require(lockDuration >= 7,"lock duration min be 7 days,")'
+        _updateReward(msg.sender);
+        totalStaked += msg.value;
+        stakeBalance[msg.sender] += msg.value;
+        uint256 newUnlock = block.timestamp + lockDuration;
+
+        if (newUnlock > unStakeTime[msg.sender]) {
+            unStakeTime[msg.sender] = block.timestamp + lockDuration;
+        }
+    }
+
+    function unstake(uint256 amount) external payable {
+        require(block.timestamp >= unStakeTime[msg.sender], "Locked");
+        require(
+            amount == stakeBalance[msg.sender],
+            "partial unstake forbidden"
+        );
+        _updateReward(msg.sender);
+        stakeBalance[msg.sender] = 0;
+        totalStaked -= amount;
+        unStakeTime[msg.sender] = 0;
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Eth Unstake Transfer failed");
+    }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
+}
---
+++ contracts/test/ApCoin.t.sol
@@ -0,0 +1,18 @@
+// // // SPDX-License-Identifier: Unlicense
+// pragma solidity ^0.8.13;
+
+// import "forge-std/Test.sol";
+// import "../src/ApCoin.sol";
+
+// contract ApCoinTest is Test {
+//     ApCoin ap;
+
+//     function setUp() public {
+//         ap = new ApCoin(address(this));
+//     }
+
+//     function testMint() public {
+//         ap.mint(msg.sender, 10);
+//         assertEq(ap.balanceOf(msg.sender), 10);
+//     }
+// }
